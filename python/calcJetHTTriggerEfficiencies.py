a3d9f8a81095e38580ef5a559745c82c696d857e
rkansal47 <raghavk1997@yahoo.co.in>
2021-06-28 22:11:14 -0700

    trig effs processor

--- python/calcJetHTTriggerEfficiencies.py
+++ python/calcJetHTTriggerEfficiencies.py
@@ -1,91 +1,115 @@
 import uproot4
-import awkward1 as ak
-import numpy as np
+import awkward as ak
 import coffea
-import coffea.hist as hist
-from copy import deepcopy
+from coffea import processor
+from coffea.nanoevents import NanoEventsFactory, BaseSchema
+from hist import Hist
 import matplotlib.pyplot as plt
 import mplhep as hep
+from glob import glob
+import pickle
 
 plt.rcParams.update({'font.size': 16})
 plt.style.use(hep.style.CMS)
 
-events = uproot4.concatenate("data/SingleMuon/Run2017*/21*/0000/nano_data2017_1.root:Events")
 
-triggers17 = [
-                'HLT_PFJet500',
-                'HLT_AK8PFJet500',
-                # 'HLT_AK8PFJet360_TrimMass30',
-                # 'HLT_AK8PFJet380_TrimMass30',
-                # 'HLT_AK8PFJet400_TrimMass30',
-                # 'HLT_AK8PFHT800_TrimMass50',
-                # 'HLT_AK8PFJet330_PFAK8BTagCSV_p17'
-            ]
+class JetHTTriggerEfficienciesProcessor(processor.ProcessorABC):
+    def __init__(self):
+        super(JetHTTriggerEfficienciesProcessor, self).__init__()
+        self.triggers = {
+            2017:   [
+                        'HLT_PFJet500',
+                        'HLT_AK8PFJet500',
+                        'HLT_AK8PFJet360_TrimMass30',
+                        'HLT_AK8PFJet380_TrimMass30',
+                        'HLT_AK8PFJet400_TrimMass30',
+                        'HLT_AK8PFHT800_TrimMass50',
+                        # 'HLT_AK8PFJet330_PFAK8BTagCSV_p17'
+                    ]
+        }
 
-jet_pts = ak.pad_none(events['FatJetAK15_pt'], 2, axis=1)
-jet_msds = ak.pad_none(events['FatJetAK15_msoftdrop'], 2, axis=1)
+        self.ptbins = [250, 300, 350, 400, 550, 1000]
+        self.msdbins = [0, 20, 40, 60, 100, 250, 500]
 
-fatjet1pt = jet_pts[:, 0]
-fatjet1msd = jet_msds[:, 0]
+        # 4b bins
+        # self.ptbins = [i for i in range(250, 401, 25)] + [450, 500, 600, 700]
+        # self.msdbins = [i for i in range(0, 241, 20)]
 
-# denominator
+    def process(self, events):
+        jet_pts = ak.pad_none(events['FatJetAK15_pt'], 2, axis=1)
+        jet_msds = ak.pad_none(events['FatJetAK15_msoftdrop'], 2, axis=1)
 
-fatjet1bool = ~ak.to_numpy(fatjet1pt).mask * ~ak.to_numpy(fatjet1msd).mask
+        fatjet1pt = jet_pts[:, 0]
+        fatjet1msd = jet_msds[:, 0]
 
-ptbins = [250, 300, 350, 400, 550, 1000]
-msdbins = [20, 40, 60, 100, 250, 500]
+        fatjet1bool = ~ak.to_numpy(fatjet1pt).mask * ~ak.to_numpy(fatjet1msd).mask  # events with at least one fat jet
 
-den = hist.Hist("Events",
-                hist.Cat("sample", "Sample"),
-                hist.Bin("pt", r"$p_T$ (GeV)", ptbins),
-                hist.Bin("msd", r"massSD (GeV)", msdbins),
-                )
+        # denominator
+        den = (
+            Hist.new
+            .Var(self.ptbins, name='pt', label="$p_T$ (GeV)")
+            .Var(self.msdbins, name='msd', label="MassSD (GeV)")
+            .Double()
+        ).fill(pt=ak.to_numpy(fatjet1pt[fatjet1bool]), msd=ak.to_numpy(fatjet1msd[fatjet1bool]))
 
-den.fill(sample='data',
-        pt=ak.to_numpy(fatjet1pt[fatjet1bool]),
-        msd=ak.to_numpy(fatjet1msd[fatjet1bool]),
+
+        # numerator
+        triggered = events[self.triggers[2017][0]]
+        for i in range(1, len(self.triggers[2017])): triggered = triggered + events[self.triggers[2017][i]]
+
+        fatjet1pt_triggered = jet_pts[:, 0][triggered * fatjet1bool]
+        fatjet1msd_triggered = jet_msds[:, 0][triggered * fatjet1bool]
+
+        num = (
+            Hist.new
+            .Var(self.ptbins, name='pt', label="$p_T (GeV)$")
+            .Var(self.msdbins, name='msd', label="MassSD (GeV)")
+            .Double()
+        ).fill(pt=ak.to_numpy(fatjet1pt_triggered), msd=ak.to_numpy(fatjet1msd_triggered))
+
+        return {
+            'den': den,
+            'num': num
+        }
+
+    def postprocess(self, accumulator):
+        return accumulator
+
+
+fileset = {'2017': glob('python/data/SingleMuon/Run2017*/21*/0000/nano_data2017_1.root')}
+
+out = processor.run_uproot_job(
+    fileset,
+    treename="Events",
+    processor_instance=JetHTTriggerEfficienciesProcessor(),
+    executor=processor.iterative_executor,
+    executor_args={'schema': BaseSchema}
 )
 
-den.values()
+out
 
-# numerator
+out['num'].values(flow=True)
 
-triggered = events[triggers17[0]]
-for i in range(1, len(triggers17)): triggered = triggered + events[triggers17[i]]
+out['den'].values(flow=True)
 
-fatjet1pt_triggered = jet_pts[:, 0][triggered]
-fatjet1msd_triggered = jet_msds[:, 0][triggered]
+effs = out['den'].copy()
+effs[:, :] = out['num'].view(flow=True) / out['den'].view(flow=True)
 
-num = hist.Hist("Events",
-                hist.Cat("sample", "Sample"),
-                hist.Bin("pt", r"$p_T$ (GeV)", ptbins),
-                hist.Bin("msd", r"massSD (GeV)", msdbins),
-                )
+effs.view()
 
-num.fill(sample='data',
-        pt=ak.to_numpy(fatjet1pt_triggered),
-        msd=ak.to_numpy(fatjet1msd_triggered),
-)
+filehandler = open('data/AK15JetHTTriggerEfficiency_2017.hist', 'wb')
+pickle.dump(effs, filehandler)
+filehandler.close()
 
-num.values()
+w, ptbins, msdbins = effs.to_numpy()
 
-# efficiencies
-
-effs = deepcopy(den)
-effs.clear()
-
-effs._sumw = {(): list(num._sumw.values())[0] / list(den._sumw.values())[0]}
-effs.label = 'Efficiency'
-
-effs.values()
-
-fig, ax = plt.subplots(1, 1, figsize=(14, 14))
-hist.plot2d(effs.sum('sample'), xaxis='msd', ax=ax)
+fig, ax = plt.subplots(figsize=(14, 14))
+mesh = ax.pcolormesh(msdbins, ptbins, w, cmap="jet")
 for i in range(len(ptbins) - 1):
     for j in range(len(msdbins) - 1):
-        ax.text((msdbins[j] + msdbins[j + 1]) / 2, (ptbins[i] + ptbins[i + 1]) / 2, effs.values()[()][i, j].round(2), color="black", ha="center", va="center", fontsize=12)
-
-plt.savefig("AK15TriggerEfficiencies.pdf")
+        ax.text((msdbins[j] + msdbins[j + 1]) / 2, (ptbins[i] + ptbins[i + 1]) / 2, w[i, j].round(2), color="black", ha="center", va="center", fontsize=12)
+ax.set_xlabel('MassSD (GeV)')
+ax.set_ylabel('$p_T$ (GeV)')
+fig.colorbar(mesh)
+plt.savefig("python/AK15TriggerEfficiencies.pdf")
 plt.show()
-
-coffea.util.save(effs, "../data/AK15JetHTTriggerEfficiency_2017.coffea")
