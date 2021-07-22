2f4d9f42064332832d1be0fe6524c0e3196eecaf
rkansal47 <raghavk1997@yahoo.co.in>
2021-07-21 17:45:50 -0700

    ak8 arg

--- processors/JetHTTriggerEfficienciesProcessor.py
+++ processors/JetHTTriggerEfficienciesProcessor.py
@@ -7,7 +7,7 @@ import numpy as np
 class JetHTTriggerEfficienciesProcessor(processor.ProcessorABC):
     """ Accumulates two 2D (pT, msd) histograms from all input events: 1) before triggers, and 2) after triggers """
 
-    def __init__(self):
+    def __init__(self, ak15=True):
         super(JetHTTriggerEfficienciesProcessor, self).__init__()
         self.muon_triggers = {
             2017:   [
@@ -31,20 +31,21 @@ class JetHTTriggerEfficienciesProcessor(processor.ProcessorABC):
                     ]
         }
 
+        self.ak15 = ak15
+
     def process(self, events):
         """ Returns pre- (den) and post- (num) trigger 2D (pT, msd) histograms from input NanoAOD events """
 
-        jet_pts = ak.pad_none(events['FatJetAK15_pt'], 2, axis=1)
-        jet_msds = ak.pad_none(events['FatJetAK15_msoftdrop'], 2, axis=1)
+        # passing single-muon triggers
+        muon_triggered = np.any(np.array([events.HLT[trigger] for trigger in self.muon_HLTs[2017]]), axis=0)
 
-        fatjet1pt = jet_pts[:, 0]
-        fatjet1msd = jet_msds[:, 0]
+        fatjets = events.FatJetAK15 if self.ak15 else events.FatJet
 
-        fatjet1bool = ~ak.to_numpy(fatjet1pt).mask * ~ak.to_numpy(fatjet1msd).mask  # events with at least one fat jet
+        # does event have a fat jet
+        fatjet1bool = ak.any(fatjets.pt, axis=1)
 
-        # passing single-muon control region triggers
-        muon_triggered = events[self.muon_triggers[2017][0]]
-        for i in range(1, len(self.muon_triggers[2017])): muon_triggered = muon_triggered + events[self.muon_triggers[2017][i]]
+        # for denominator select events which pass the muon triggers and contain at least one fat jet
+        den_selection = fatjet1bool * muon_triggered
 
         # denominator
         den = (
@@ -52,21 +53,25 @@ class JetHTTriggerEfficienciesProcessor(processor.ProcessorABC):
             .Reg(50, 0, 1000, name='pt', label="$p_T (GeV)$")
             .Reg(15, 0, 300, name='msd', label="MassSD (GeV)")
             .Double()
-        ).fill(pt=ak.to_numpy(fatjet1pt[fatjet1bool * muon_triggered]), msd=ak.to_numpy(fatjet1msd[fatjet1bool * muon_triggered]))
+        ).fill( jet1pt=fatjets.pt[den_selection][:, 0].to_numpy(),
+                jet1msd=fatjets.msoftdrop[den_selection][:, 0].to_numpy()
+            )
 
         # numerator
-        triggered = events[self.triggers[2017][0]]
-        for i in range(1, len(self.triggers[2017])): triggered = triggered + events[self.triggers[2017][i]]
 
-        fatjet1pt_triggered = jet_pts[:, 0][triggered * fatjet1bool * muon_triggered]
-        fatjet1msd_triggered = jet_msds[:, 0][triggered * fatjet1bool * muon_triggered]
+        # passing all HLT triggers
+        bbVV_triggered = np.any(np.array([events.HLT[trigger] for trigger in self.HLTs[2017]]), axis=0)
+
+        num_selection = fatjet1bool * muon_triggered * bbVV_triggered
 
         num = (
             Hist.new
             .Reg(50, 0, 1000, name='pt', label="$p_T (GeV)$")
             .Reg(15, 0, 300, name='msd', label="MassSD (GeV)")
             .Double()
-        ).fill(pt=ak.to_numpy(fatjet1pt_triggered), msd=ak.to_numpy(fatjet1msd_triggered))
+        ).fill( jet1pt=fatjets.pt[num_selection][:, 0].to_numpy(),
+                jet1msd=fatjets.msoftdrop[num_selection][:, 0].to_numpy(),
+              )
 
         return {
             'den': den,
@@ -81,7 +86,7 @@ class JetHTTriggerEfficienciesProcessor(processor.ProcessorABC):
 class JetHT3DTriggerEfficienciesProcessor(processor.ProcessorABC):
     """ Accumulates two 2D (pT, msd) histograms from all input events: 1) before triggers, and 2) after triggers """
 
-    def __init__(self):
+    def __init__(self, ak15=True):
         super(JetHT3DTriggerEfficienciesProcessor, self).__init__()
 
         self.muon_HLTs = {
@@ -106,6 +111,8 @@ class JetHT3DTriggerEfficienciesProcessor(processor.ProcessorABC):
                     ]
         }
 
+        self.ak15 = ak15
+
 
     def process(self, events):
         """ Returns pre- (den) and post- (num) trigger 3D (jet 2 pT, jet 1 pT, jet 1 msd) histograms from input NanoAOD events """
@@ -113,8 +120,10 @@ class JetHT3DTriggerEfficienciesProcessor(processor.ProcessorABC):
         # passing single-muon triggers
         muon_triggered = np.any(np.array([events.HLT[trigger] for trigger in self.muon_HLTs[2017]]), axis=0)
 
+        fatjets = events.FatJetAK15 if self.ak15 else events.FatJet
+
         # does event have a fat jet
-        fatjet1bool = ak.any(events.FatJetAK15.pt, axis=1)
+        fatjet1bool = ak.any(fatjets.pt, axis=1)
 
         # for denominator select events which pass the muon triggers and contain at least one fat jet
         den_selection = fatjet1bool * muon_triggered
@@ -128,9 +137,9 @@ class JetHT3DTriggerEfficienciesProcessor(processor.ProcessorABC):
             .Reg(50, 0, 1000, name='jet1pt', label="AK15 Fat Jet 1 $p_T$ (GeV)")
             .Reg(15, 0, 300, name='jet1msd', label="AK15 Fat Jet 1 MassSD (GeV)")
             .Double()
-        ).fill( jet1pt=events.FatJetAK15.pt[den_selection][:, 0].to_numpy(),
-                jet1msd=events.FatJetAK15.msoftdrop[den_selection][:, 0].to_numpy(),
-                jet2pt=ak.fill_none(ak.pad_none(events.FatJetAK15.pt[den_selection][:, 1:2], 1, axis=1), 0).to_numpy()[:, 0],  # putting events with no fat jet 2 in the low pT bin
+        ).fill( jet1pt=fatjets.pt[den_selection][:, 0].to_numpy(),
+                jet1msd=fatjets.msoftdrop[den_selection][:, 0].to_numpy(),
+                jet2pt=ak.fill_none(ak.pad_none(fatjets.pt[den_selection][:, 1:2], 1, axis=1), 0).to_numpy()[:, 0],  # putting events with no fat jet 2 in the low pT bin
               )
 
         # numerator
@@ -146,11 +155,11 @@ class JetHT3DTriggerEfficienciesProcessor(processor.ProcessorABC):
             .Reg(50, 0, 1000, name='jet1pt', label="AK15 Fat Jet 1 $p_T$ (GeV)")
             .Reg(15, 0, 300, name='jet1msd', label="AK15 Fat Jet 1 MassSD (GeV)")
             .Double()
-        ).fill( jet1pt=events.FatJetAK15.pt[num_selection][:, 0].to_numpy(),
-                jet1msd=events.FatJetAK15.msoftdrop[num_selection][:, 0].to_numpy(),
-                jet2pt=ak.fill_none(ak.pad_none(events.FatJetAK15.pt[num_selection][:, 1:2], 1, axis=1), 0).to_numpy()[:, 0],  # putting events with no fat jet 2 in the low pT bin
+        ).fill( jet1pt=fatjets.pt[num_selection][:, 0].to_numpy(),
+                jet1msd=fatjets.msoftdrop[num_selection][:, 0].to_numpy(),
+                jet2pt=ak.fill_none(ak.pad_none(fatjets.pt[num_selection][:, 1:2], 1, axis=1), 0).to_numpy()[:, 0],  # putting events with no fat jet 2 in the low pT bin
               )
-              
+
         return {
             'den': den,
             'num': num
