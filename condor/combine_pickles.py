9ec1ee65df9f86f95f3dd0b37640352b6ea725c5
rkansal47 <raghavk1997@yahoo.co.in>
2021-08-18 22:36:29 -0500

    bugs

--- condor/combine_pickles.py
+++ condor/combine_pickles.py
@@ -28,7 +28,7 @@ def accumulate_files(files: list, norm: bool = False, convert_to_dict: bool = Fa
     for year, datasets in out.items():
         for dataset, output in datasets.items():
             if norm:
-                output['skimmed_events']['weight'] = column_accumulator(output['skimmed_events']['weight'].values / output['nevents'])
+                output['skimmed_events']['weight'] = column_accumulator(output['skimmed_events']['weight'].value / output['nevents'])
 
             if convert_to_dict:
                 output['skimmed_events'] = {
@@ -48,52 +48,54 @@ if __name__ == "__main__":
     parser.add_argument('--samples',    default=[],           help='if --r, which samples in indir to combine - if blank (default) will go through all',     nargs='*')
     parser.add_argument('--norm',      default=False,              help="divide weight by total events", type=bool)
     parser.add_argument('--combine-further',      default=True,              help="combine already combined pickles into broader categories ('HHbbVV4q', 'QCD', 'Top', 'V', 'Data')", type=bool)
+    parser.add_argument('--combine-further-only',      default=False,              help="only perform the combine further step", type=bool)
     args = parser.parse_args()
 
 
-    if args.r:
-        dirs = [args.indir + '/' + dir for dir in listdir(args.indir)]
+    if not args.combine_further_only:
+        if args.r:
+            dirs = [args.indir + '/' + dir for dir in listdir(args.indir) if os.path.isdir(args.indir + '/' + dir)]
 
-        if args.name: args.name = '_' + args.name
-        acc_str = "_column_accs" if args.combine_further else ""  # to remind that these files have coffea `column_accumulators`, not arrays
+            if args.name: args.name = '_' + args.name
+            acc_str = "_column_accs" if args.combine_further else ""  # to remind that these files have coffea `column_accumulators`, not arrays
 
-        for sample in dirs:
-            sample_name = sample.split('/')[-1]
+            for sample in dirs:
+                sample_name = sample.split('/')[-1]
 
-            files = [sample + '/' + file for file in listdir(sample)]
+                files = [sample + '/' + file for file in listdir(sample)]
+                if not len(files):
+                    print(f"No files for {sample_name}")
+                    continue
+
+                print(f"Accumulating {sample_name}")
+                accumulated = accumulate_files(files, norm = args.norm and 'JetHT' not in sample_name, convert_to_dict = not args.combine_further)  # normalize if not data, and don't convert to dict if combining further
+
+                with open(f"{args.indir}/{sample_name}{args.name}{acc_str}.pkl", 'wb') as file:
+                    pickle.dump(accumulated, file)
+
+                print(f"Saved as {args.indir}/{sample_name}{args.name}.pkl")
+        else:
+            if not args.name:
+                print("Name can't be blank -- exiting")
+                sys.exit()
+
+            files = [args.indir + '/' + file for file in listdir(args.indir)]
             if not len(files):
-                print(f"No files for {sample_name}")
-                continue
+                print("No files to accumulate -- exiting")
+                sys.exit()
 
-            print(f"Accumulating {sample_name}")
-            accumulated = accumulate_files(files, norm = args.norm and 'JetHT' not in sample_name, convert_to_dict = not args.combine_further)  # normalize if not data, and don't convert to dict if combining further
+            print(f"Accumulating {args.indir}")
+            accumulated = accumulate_files(files, norm=args.norm, convert_to_dict=True)
 
-            with open(f"{args.indir}/{sample_name}{args.name}{acc_str}.pkl", 'wb') as file:
+            with open(f"{args.indir}/{args.name}.pkl", 'wb') as file:
                 pickle.dump(accumulated, file)
 
-            print(f"Saved as {args.indir}/{sample_name}{args.name}.pkl")
-    else:
-        if not args.name:
-            print("Name can't be blank -- exiting")
-            sys.exit()
-
-        files = [args.indir + '/' + file for file in listdir(args.indir)]
-        if not len(files):
-            print("No files to accumulate -- exiting")
-            sys.exit()
-
-        print(f"Accumulating {args.indir}")
-        accumulated = accumulate_files(files, norm=args.norm, convert_to_dict=True)
-
-        with open(f"{args.indir}/{args.name}.pkl", 'wb') as file:
-            pickle.dump(accumulated, file)
-
-        print(f"Saved as {args.indir}/{args.name}.pkl")
+            print(f"Saved as {args.indir}/{args.name}.pkl")
 
 
     if args.combine_further:
         os.system(f'mkdir -p {args.indir}/{args.year}_combined{args.name}')
-        pickles = [args.indir + '/' + pickle for pickle in listdir(args.indir) if pickle[-4:] == '.pkl']
+        pickles = [args.indir + '/' + pickle for pickle in listdir(args.indir) if pickle.endswith(f'{args.name}_column_accs.pkl')]
 
         combiner = {
             'HHbbVV4q': [pickle for pickle in pickles if f'{args.year}_HHToBBVVToBBQQQQ' in pickle],
@@ -104,10 +106,10 @@ if __name__ == "__main__":
         }
 
         for key, files in combiner.items():
-            print(f"Accumulating {key}: {files}")
+            print(f"Accumulating {key}: {files}\n")
 
             accumulated = accumulate_files(files, norm=False, convert_to_dict=True)
             with open(f"{args.indir}/{args.year}_combined{args.name}/{key}.pkl", 'wb') as file:
                 pickle.dump(accumulated, file)
 
-            print(f"Saved as {args.indir}/{args.year}_combined{args.name}/{key}.pkl")
+            print(f"Saved as {args.indir}/{args.year}_combined{args.name}/{key}.pkl\n")
