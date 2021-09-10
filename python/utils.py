2114285d1f82afa72c71acb9dd4f066c11a5855b
rkansal47 <raghavk1997@yahoo.co.in>
2021-09-09 19:58:49 -0700

    bdt training script advancement

--- python/utils.py
+++ python/utils.py
@@ -2,6 +2,20 @@ import vector
 from hist import Hist
 
 
+def add_bool_arg(parser, name, help, default=False, no_name=None):
+    """ Add a boolean command line argument for argparse """
+    varname = '_'.join(name.split('-'))  # change hyphens to underscores
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('--' + name, dest=varname, action='store_true', help=help)
+    if(no_name is None):
+        no_name = 'no-' + name
+        no_help = "don't " + help
+    else:
+        no_help = help
+    group.add_argument('--' + no_name, dest=varname, action='store_false', help=no_help)
+    parser.set_defaults(**{varname: default})
+
+
 def make_vector(events: dict, name: str, mask=None):
     """
     Creates Lorentz vector from input events and beginning name, assuming events contain {name}Pt, {name}Phi, {name}Eta, {Name}Msd variables
@@ -60,7 +74,7 @@ def singleVarHist(events: dict, var: str, bins: list, label: str, weight_key: st
         label (str): label for variable (shows up when plotting)
         weight_key (str, optional): which weight to use from events, if different from 'weight'
     """
-    
+
     keys = list(events.keys())
 
     h = (
