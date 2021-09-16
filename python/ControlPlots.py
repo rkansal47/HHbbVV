376fb3f01033b4f22f1cabb5e161facf28c370f9
rkansal47 <raghavk1997@yahoo.co.in>
2021-09-15 19:25:00 -0700

    yields analysis

--- python/ControlPlots.py
+++ python/ControlPlots.py
@@ -15,15 +15,16 @@ import plotting
 import pickle
 
 # backgrounds listed first and plotted in order
-keys = ['V', 'Top', 'QCD', 'Data', 'HHbbVV4q']
-labels = ['VV/V+jets', 'ST/TT', 'QCD', 'Data', 'HHbbVV4q']
+keys = ["V", "Top", "QCD", "Data", "HHbbVV4q"]
+labels = ["VV/V+jets", "ST/TT", "QCD", "Data", "HHbbVV4q"]
 num_bg = 3  # up to this label for bg
-sig = 'HHbbVV4q'
-data_path = '../../data/2017_combined/'
+sig = "HHbbVV4q"
+data_path = "../../data/2017_combined/"
 
 import os
-plotdir = '../plots/ControlPlots/Sep3/'
-os.system(f'mkdir -p {plotdir}')
+
+plotdir = "../plots/ControlPlots/Sep3/"
+os.system(f"mkdir -p {plotdir}")
 
 
 # import importlib
@@ -36,8 +37,8 @@ events = {}
 for key in keys:
     # if key != sig: continue
     print(key)
-    with open(f'{data_path}{key}.pkl', 'rb') as file:
-        events[key] = pickle.load(file)['skimmed_events']
+    with open(f"{data_path}{key}.pkl", "rb") as file:
+        events[key] = pickle.load(file)["skimmed_events"]
 
 for key in keys:
     print(f"{key} events: {np.sum(events[key]['weight']):.2f}")
@@ -52,16 +53,16 @@ dR = 1
 for key in keys:
     # if key != sig: continue
     print(key)
-    jet1_bb_leading = events[key]['ak8FatJetParticleNetMD_Txbb'][:, 0:1] >= events[key]['ak8FatJetParticleNetMD_Txbb'][:, 1:2]
+    jet1_bb_leading = events[key]["ak8FatJetParticleNetMD_Txbb"][:, 0:1] >= events[key]["ak8FatJetParticleNetMD_Txbb"][:, 1:2]
     bb_mask = np.concatenate([jet1_bb_leading, ~jet1_bb_leading], axis=1)
 
-    jet1_VV_leading = events[key]['ak15FatJetParticleNet_Th4q'][:, 0:1] >= events[key]['ak15FatJetParticleNet_Th4q'][:, 1:2]
+    jet1_VV_leading = events[key]["ak15FatJetParticleNet_Th4q"][:, 0:1] >= events[key]["ak15FatJetParticleNet_Th4q"][:, 1:2]
     VV_mask = ak.concatenate([jet1_VV_leading, ~jet1_VV_leading], axis=1)
 
     print("prelim masks")
 
-    ak8FatJet = utils.make_vector(events[key], 'ak8FatJet')
-    ak15FatJet = utils.make_vector(events[key], 'ak15FatJet')
+    ak8FatJet = utils.make_vector(events[key], "ak8FatJet")
+    ak15FatJet = utils.make_vector(events[key], "ak15FatJet")
 
     print("fat jet arrays")
 
@@ -71,7 +72,7 @@ for key in keys:
 
     # overlap policy is: if bb and VV candidate jets overlap, use the ak15 jet which is farthest from the bb jet as the VV candidate
     bb_cand_VV_not_cand_dist = ak8FatJet[bb_mask].deltaR(ak15FatJet[~VV_mask])
-    VV_not_cand_farther =  bb_cand_VV_not_cand_dist > bb_cand_VV_cand_dist
+    VV_not_cand_farther = bb_cand_VV_not_cand_dist > bb_cand_VV_cand_dist
 
     # flip VV_mask only if (VV candidate jet is overlapping AND non-candidate jet is farther away)
     final_VV_mask = VV_mask ^ (VV_cand_overlap * VV_not_cand_farther)
@@ -82,11 +83,11 @@ for key in keys:
     values = events[key].values()
 
     for var, value in list(zip(vars, values)):
-        if var.startswith('ak8FatJet'):
-            newvar = 'bb' + var.split('ak8')[1]
+        if var.startswith("ak8FatJet"):
+            newvar = "bb" + var.split("ak8")[1]
             events[key][newvar] = value[bb_mask]
-        elif var.startswith('ak15FatJet'):
-            newvar = 'VV' + var.split('ak15')[1]
+        elif var.startswith("ak15FatJet"):
+            newvar = "VV" + var.split("ak15")[1]
             events[key][newvar] = value[final_VV_mask]
 
 
@@ -96,17 +97,17 @@ for key in keys:
 
 for key in keys:
     print(key)
-    bbFatJet = utils.make_vector(events[key], 'bbFatJet')
-    VVFatJet = utils.make_vector(events[key], 'VVFatJet')
+    bbFatJet = utils.make_vector(events[key], "bbFatJet")
+    VVFatJet = utils.make_vector(events[key], "VVFatJet")
     Dijet = bbFatJet + VVFatJet
 
-    events[key]['DijetPt'] = Dijet.pt
-    events[key]['DijetMass'] = Dijet.M
-    events[key]['DijetEta'] = Dijet.eta
+    events[key]["DijetPt"] = Dijet.pt
+    events[key]["DijetMass"] = Dijet.M
+    events[key]["DijetEta"] = Dijet.eta
 
-    events[key]['bbFatJetPtOverDijetPt'] = events[key]['bbFatJetPt'] / events[key]['DijetPt']
-    events[key]['VVFatJetPtOverDijetPt'] = events[key]['VVFatJetPt'] / events[key]['DijetPt']
-    events[key]['VVFatJetPtOverbbFatJetPt'] = events[key]['VVFatJetPt'] / events[key]['bbFatJetPt']
+    events[key]["bbFatJetPtOverDijetPt"] = events[key]["bbFatJetPt"] / events[key]["DijetPt"]
+    events[key]["VVFatJetPtOverDijetPt"] = events[key]["VVFatJetPt"] / events[key]["DijetPt"]
+    events[key]["VVFatJetPtOverbbFatJetPt"] = events[key]["VVFatJetPt"] / events[key]["bbFatJetPt"]
 
     # ak8FatJet = utils.make_vector(events[key], 'ak8FatJet')
     # ak15FatJet = utils.make_vector(events[key], 'ak15FatJet')
@@ -128,27 +129,29 @@ for key in keys:
 
 from coffea.lookup_tools.dense_lookup import dense_lookup
 
-with open('../corrections/trigEffs/AK15JetHTTriggerEfficiency_2017.hist', 'rb') as filehandler:
+with open("../corrections/trigEffs/AK15JetHTTriggerEfficiency_2017.hist", "rb") as filehandler:
     ak15TrigEffs = pickle.load(filehandler)
 
 ak15TrigEffsLookup = dense_lookup(np.nan_to_num(ak15TrigEffs.view(flow=False), 0), np.squeeze(ak15TrigEffs.axes.edges))
 
-with open('../corrections/trigEffs/AK8JetHTTriggerEfficiency_2017.hist', 'rb') as filehandler:
+with open("../corrections/trigEffs/AK8JetHTTriggerEfficiency_2017.hist", "rb") as filehandler:
     ak8TrigEffs = pickle.load(filehandler)
 
 ak8TrigEffsLookup = dense_lookup(np.nan_to_num(ak8TrigEffs.view(flow=False), 0), np.squeeze(ak8TrigEffs.axes.edges))
 
 for key in keys:
-    if key == 'Data':
-        events[key]['finalWeight'] = events[key]['weight']
+    if key == "Data":
+        events[key]["finalWeight"] = events[key]["weight"]
     else:
-        bb_fj_trigEffs = ak8TrigEffsLookup(events[key]['bbFatJetPt'], events[key]['bbFatJetMsd'])
-        VV_fj_trigEffs = ak15TrigEffsLookup(events[key]['VVFatJetPt'], events[key]['VVFatJetMsd'])
+        bb_fj_trigEffs = ak8TrigEffsLookup(events[key]["bbFatJetPt"], events[key]["bbFatJetMsd"])
+        VV_fj_trigEffs = ak15TrigEffsLookup(events[key]["VVFatJetPt"], events[key]["VVFatJetMsd"])
         combined_trigEffs = 1 - (1 - bb_fj_trigEffs) * (1 - VV_fj_trigEffs)
-        events[key]['finalWeight'] = events[key]['weight'] * combined_trigEffs
+        events[key]["finalWeight"] = events[key]["weight"] * combined_trigEffs
 
-QCD_SCALE_FACTOR =  (np.sum(events['Data']['finalWeight']) - np.sum(events['Top']['finalWeight']) - np.sum(events['V']['finalWeight'])) / (np.sum(events['QCD']['finalWeight']))
-events['QCD']['finalWeight'] *= QCD_SCALE_FACTOR
+QCD_SCALE_FACTOR = (np.sum(events["Data"]["finalWeight"]) - np.sum(events["Top"]["finalWeight"]) - np.sum(events["V"]["finalWeight"])) / (
+    np.sum(events["QCD"]["finalWeight"])
+)
+events["QCD"]["finalWeight"] *= QCD_SCALE_FACTOR
 
 for key in keys:
     print(f"Final {key} events: {np.sum(events[key]['finalWeight']):.2f}")
@@ -161,11 +164,11 @@ for key in keys:
 for key in keys:
     # if key != sig: continue
     print(key)
-    with open(f'{data_path}{key}.pkl', 'rb') as file:
+    with open(f"{data_path}{key}.pkl", "rb") as file:
         out_pickle = pickle.load(file)
 
-    out_pickle['skimmed_events'] = events[key]
-    with open(f'{data_path}{key}.pkl', 'wb') as file:
+    out_pickle["skimmed_events"] = events[key]
+    with open(f"{data_path}{key}.pkl", "wb") as file:
         pickle.dump(out_pickle, file)
 
 
@@ -173,32 +176,27 @@ for key in keys:
 # Control plots
 ##################################################################################
 
-sig_scale = np.sum(events['Data']['finalWeight']) / np.sum(events['HHbbVV4q']['finalWeight'])
+sig_scale = np.sum(events["Data"]["finalWeight"]) / np.sum(events["HHbbVV4q"]["finalWeight"])
 
 
 hists = {}
 
 hist_vars = {  # (bins, labels)
-    'MET_pt': ([50, 0, 250], r"$p^{miss}_T$ (GeV)"),
-
-    'DijetEta': ([50, -8, 8], r"$\eta^{jj}$"),
-    'DijetPt': ([50, 0, 750], r"$p_T^{jj}$ (GeV)"),
-    'DijetMass': ([50, 0, 2500], r"$m^{jj}$ (GeV)"),
-
-    'bbFatJetEta': ([50, -3, 3], r"$\eta^{bb}$"),
-    'bbFatJetPt': ([50, 200, 1000], r"$p^{bb}_T$ (GeV)"),
-    'bbFatJetMsd': ([50, 20, 250], r"$m^{bb}$ (GeV)"),
-    'bbFatJetParticleNetMD_Txbb': ([50, 0, 1], r"$p^{bb}_{Txbb}$"),
-
-    'VVFatJetEta': ([50, -3, 3], r"$\eta^{VV}$"),
-    'VVFatJetPt': ([50, 200, 1000], r"$p^{VV}_T$ (GeV)"),
-    'VVFatJetMsd': ([50, 20, 500], r"$m^{VV}$ (GeV)"),
-    'VVFatJetParticleNet_Th4q': ([50, 0, 1], r"$p^{VV}_{Th4q}$"),
-
-    'bbFatJetPtOverDijetPt': ([50, 0, 40], r"$p^{bb}_T / p_T^{jj}$"),
-    'VVFatJetPtOverDijetPt': ([50, 0, 40], r"$p^{VV}_T / p_T^{jj}$"),
-    'VVFatJetPtOverbbFatJetPt': ([50, 0.4, 2.5], r"$p^{VV}_T / p^{bb}_T$"),
-
+    "MET_pt": ([50, 0, 250], r"$p^{miss}_T$ (GeV)"),
+    "DijetEta": ([50, -8, 8], r"$\eta^{jj}$"),
+    "DijetPt": ([50, 0, 750], r"$p_T^{jj}$ (GeV)"),
+    "DijetMass": ([50, 0, 2500], r"$m^{jj}$ (GeV)"),
+    "bbFatJetEta": ([50, -3, 3], r"$\eta^{bb}$"),
+    "bbFatJetPt": ([50, 200, 1000], r"$p^{bb}_T$ (GeV)"),
+    "bbFatJetMsd": ([50, 20, 250], r"$m^{bb}$ (GeV)"),
+    "bbFatJetParticleNetMD_Txbb": ([50, 0, 1], r"$p^{bb}_{Txbb}$"),
+    "VVFatJetEta": ([50, -3, 3], r"$\eta^{VV}$"),
+    "VVFatJetPt": ([50, 200, 1000], r"$p^{VV}_T$ (GeV)"),
+    "VVFatJetMsd": ([50, 20, 500], r"$m^{VV}$ (GeV)"),
+    "VVFatJetParticleNet_Th4q": ([50, 0, 1], r"$p^{VV}_{Th4q}$"),
+    "bbFatJetPtOverDijetPt": ([50, 0, 40], r"$p^{bb}_T / p_T^{jj}$"),
+    "VVFatJetPtOverDijetPt": ([50, 0, 40], r"$p^{VV}_T / p_T^{jj}$"),
+    "VVFatJetPtOverbbFatJetPt": ([50, 0.4, 2.5], r"$p^{VV}_T / p^{bb}_T$"),
     # 'ak8DijetEta': ([50, -5, 5], r"$\eta^{jj}$"),
     # 'ak8DijetPt': ([50, 0, 2000], r"$p_T^{jj}$ (GeV)"),
     # 'ak8DijetMass': ([50, 0, 2000], r"$m^{jj}$ (GeV)"),
@@ -210,7 +208,7 @@ hist_vars = {  # (bins, labels)
 
 
 for var, (bins, label) in hist_vars.items():
-    hists[var] = utils.singleVarHist(events, var, bins, label, weight_key='finalWeight')
+    hists[var] = utils.singleVarHist(events, var, bins, label, weight_key="finalWeight")
 
 # var = 'MET_pt'
 
