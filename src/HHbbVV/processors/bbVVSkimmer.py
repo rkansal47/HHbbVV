f8d088723c6290dfa4f5ee916ff32316192d7b17
rkansal47 <raghavk1997@yahoo.co.in>
2021-11-10 15:04:56 -0800

    bug fix for inference when there aren't two ak15 fat jets in the event

--- src/HHbbVV/processors/bbVVSkimmer.py
+++ src/HHbbVV/processors/bbVVSkimmer.py
@@ -117,10 +117,19 @@ class bbVVSkimmer(ProcessorABC):
         )
 
     def pad_val(
-        self, arr: ak.Array, target: int, value: float, axis: int = 0, to_numpy: bool = True
+        self,
+        arr: ak.Array,
+        target: int,
+        value: float,
+        axis: int = 0,
+        to_numpy: bool = True,
+        clip: bool = True,
     ):
-        """pads awkward array up to `target` index along axis `axis` with value `value`, optionally converts to numpy array"""
-        ret = ak.fill_none(ak.pad_none(arr, target, axis=axis, clip=True), value)
+        """
+        pads awkward array up to ``target`` index along axis ``axis`` with value ``value``,
+        optionally converts to numpy array
+        """
+        ret = ak.fill_none(ak.pad_none(arr, target, axis=axis, clip=clip), value, axis=None)
         return ret.to_numpy() if to_numpy else ret
 
     def add_selection(self, name: str, sel: np.ndarray, selection: PackedSelection, cutflow: dict):
