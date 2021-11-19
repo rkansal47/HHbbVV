546a0e97d6c774658869d57fc3814102c10fb157
rkansal47 <raghavk1997@yahoo.co.in>
2021-11-19 13:20:58 -0800

    chunksize and executor args

--- src/condor/submit.templ.sh
+++ src/condor/submit.templ.sh
@@ -8,7 +8,7 @@ mkdir outfiles
 
 # run code
 pip install --user onnxruntime
-python SCRIPTNAME --year YEAR --starti STARTNUM --endi ENDNUM --samples SAMPLE --processor PROCESSOR --condor
+python SCRIPTNAME --year YEAR --starti STARTNUM --endi ENDNUM --samples SAMPLE --processor PROCESSOR
 
 #move output to eos
 xrdcp -f outfiles/* EOSOUT
