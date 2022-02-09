dec24ab6f19903867bad6911daec6046cda5b36b
rkansal47 <raghavk1997@yahoo.co.in>
2022-02-09 12:55:10 -0800

    hh4v gen selection

--- src/HHbbVV/processors/utils.py
+++ src/HHbbVV/processors/utils.py
@@ -4,7 +4,10 @@ Common functions for processors.
 Author(s): Raghav Kansal
 """
 
+import numpy as np
 import awkward as ak
+
+from coffea.analysis_tools import PackedSelection
 
 
 def pad_val(
@@ -21,3 +24,22 @@ def pad_val(
     """
     ret = ak.fill_none(ak.pad_none(arr, target, axis=axis, clip=clip), value, axis=None)
     return ret.to_numpy() if to_numpy else ret
+
+
+def add_selection(
+    self,
+    name: str,
+    sel: np.ndarray,
+    selection: PackedSelection,
+    cutflow: dict,
+    isData: bool,
+    signGenWeights: ak.Array,
+):
+    """adds selection to PackedSelection object and the cutflow dictionary"""
+    selection.add(name, sel)
+    cutflow[name] = (
+        np.sum(selection.all(*selection.names))
+        if isData
+        # add up sign of genWeights for MC
+        else np.sum(signGenWeights[selection.all(*selection.names)])
+    )
