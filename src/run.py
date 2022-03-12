e302dde48a8be5a92807248da66514d5ebd3436e
Cristina <criss.ms7@gmail.com>
2022-03-11 19:25:22 -0600

    add input producer to run

--- src/run.py
+++ src/run.py
@@ -83,74 +83,6 @@ def get_fileset(processor, year, samples, subsamples, starti, endi):
 
     return fileset
 
-
-# def get_fileset(ptype, samples, starti, endi):
-#     if ptype == "trigger":
-#         with open("data/SingleMuon_2017.txt", "r") as file:
-#             filelist = [f[:-1] for f in file.readlines()]
-#
-#         files = {"2017": filelist}
-#         fileset = {k: files[k][starti:endi] for k in files.keys()}
-#         return fileset
-#
-#     elif ptype == "skimmer":
-#         from os import listdir
-#
-#         fileset = {}
-#
-#         # if "2017_HHToBBVVToBBQQQQ_cHHH1" in samples:
-#         #     # TODO: replace with UL sample once we have it
-#         #     with open("data/2017_preUL_nano/HHToBBVVToBBQQQQ_cHHH1.txt", "r") as file:
-#         #         filelist = [
-#         #             f[:-1].replace("/eos/uscms/", "root://cmsxrootd.fnal.gov//")
-#         #             for f in file.readlines()
-#         #         ]  # need to use xcache redirector at Nebraksa coffea-casa
-#         #
-#         #     fileset["2017_HHToBBVVToBBQQQQ_cHHH1"] = filelist[starti:endi]
-#
-#         if "2017_GluGluToHHTo4V_node_cHHH1" in samples:
-#             # TODO: replace with UL sample once we have it
-#             with open(
-#                 "data/2017_preUL_nano/GluGluToHHTo4V_node_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8.txt",
-#                 "r",
-#             ) as file:
-#                 filelist = [
-#                     f[:-1].replace("/eos/uscms/", "root://cmsxrootd.fnal.gov//")
-#                     for f in file.readlines()
-#                 ]  # need to use xcache redirector at Nebraksa coffea-casa
-#
-#             fileset["2017_GluGluToHHTo4V_node_cHHH1"] = filelist[starti:endi]
-#
-#         # extra samples in the folder we don't need for this analysis -
-#         # TODO: should instead have a list of all samples we need
-#         ignore_samples = [
-#             "GluGluHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8",
-#             "GluGluHToWWToLNuQQ_M125_TuneCP5_PSweight_13TeV-powheg2-jhugen727-pythia8",
-#             "ST_tW_antitop_5f_DS_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8",
-#             "ST_tW_top_5f_DS_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8",
-#         ]
-#
-#         for sample in listdir("data/2017_UL_nano/"):
-#             if sample[-4:] == ".txt" and sample[:-4] not in ignore_samples:
-#                 if "2017_" + sample[:-4].split("_TuneCP5")[0] in samples:
-#                     with open(f"data/2017_UL_nano/{sample}", "r") as file:
-#                         if "JetHT" in sample:
-#                             filelist = [
-#                                 f[:-1].replace("/hadoop/cms/", "root://redirector.t2.ucsd.edu//")
-#                                 for f in file.readlines()
-#                             ]
-#                         else:
-#                             filelist = [
-#                                 f[:-1].replace("/eos/uscms/", "root://cmsxrootd.fnal.gov//")
-#                                 for f in file.readlines()
-#                             ]
-#
-#                     fileset["2017_" + sample[:-4].split("_TuneCP5")[0]] = filelist[starti:endi]
-#
-#         print(fileset)
-#         return fileset
-
-
 def get_xsecs():
     with open("data/xsecs.json") as f:
         xsecs = json.load(f)
@@ -160,7 +92,6 @@ def get_xsecs():
             xsecs[key] = eval(value)
 
     return xsecs
-
 
 def main(args):
 
@@ -173,6 +104,9 @@ def main(args):
         from HHbbVV.processors import bbVVSkimmer
 
         p = bbVVSkimmer(xsecs=get_xsecs())
+    elif args.processor == "input":
+        from HHbbVV.processors import TaggerInputSkimmer
+        p = TaggerInputSkimmer.TaggerInputSkimmer()
 
     fileset = get_fileset(
         args.processor, args.year, args.samples, args.subsamples, args.starti, args.endi
@@ -269,15 +203,12 @@ if __name__ == "__main__":
     parser.add_argument("--year", default="2017", help="year", type=str)
     parser.add_argument("--starti", default=0, help="start index of files", type=int)
     parser.add_argument("--endi", default=-1, help="end index of files", type=int)
-    # parser.add_argument(
-    #     "--outdir", dest="outdir", default="outfiles", help="directory for output files", type=str
-    # )
     parser.add_argument(
         "--processor",
         default="trigger",
         help="Trigger processor",
         type=str,
-        choices=["trigger", "skimmer"],
+        choices=["trigger", "skimmer", "input"],
     )
     parser.add_argument(
         "--executor",
