084f76e1be7599c21381a352fc0c85a78e8403af
rkansal47 <raghavk1997@yahoo.co.in>
2022-03-14 18:00:15 -0700

    gen res and gen X variables

--- src/HHbbVV/processors/GenSelection.py
+++ src/HHbbVV/processors/GenSelection.py
@@ -246,16 +246,18 @@ def tagger_gen_H_matching(
     # select event only if distance to closest higgs is < ``match_dR``
     matched_higgs_mask = ak.any(fatjets.delta_r(matched_higgs) < match_dR, axis=1)
     # higgs kinematics
-    genVars = {
+    genResVars = {
         f"fj_genRes_{key}": ak.fill_none(matched_higgs[var], -99999) for (var, key) in P4.items()
     }
+    # Higgs parent kinematics
+    bulkg = matched_higgs.distinctParent
+    genXVars = {f"fj_genX_{key}": ak.fill_none(bulkg[var], -99999) for (var, key) in P4.items()}
+
+    genVars = {**genResVars, **genXVars}
 
     matched_higgs_children = matched_higgs.children
 
     if "VV" in decays:
-        # select only VV children
-        children_mask = get_pid_mask(matched_higgs_children, [W_PDGID,Z_PDGID], byall=False)
-        matched_higgs_children = matched_higgs_children[children_mask]
         children_mass = matched_higgs_children.mass
 
         # select lower mass child as V* and higher as V
@@ -269,6 +271,10 @@ def tagger_gen_H_matching(
         matched_Vs_mask = ak.any(fatjets.delta_r(v) < jet_dR, axis=1) & ak.any(
             fatjets.delta_r(v_star) < jet_dR, axis=1
         )
+
+        # I think this will find all VV daughters - not just the ones from the Higgs matched to the fatjet? (e.g. for HH4W it'll find all 8 daughters?)
+        # daughter_mask = get_pid_mask(genparts.distinctParent, [W_PDGID, Z_PDGID], ax=1, byall=False)
+        # daughters = genparts[daughter_mask & genparts.hasFlags(GEN_FLAGS)]
 
         # get VV daughters
         daughters = ak.flatten(ak.flatten(matched_higgs_children.distinctChildren, axis=2), axis=2)
@@ -303,7 +309,7 @@ def tagger_gen_H_matching(
             "fj_H_VV_elenuqq": to_label(decay == 4),
             "fj_H_VV_munuqq": to_label(decay == 6),
             "fj_H_VV_taunuqq": to_label(decay == 8),
-            "fj_H_VV_unmatched": to_label(matched_higgs_mask & ~matched_Vs_mask & ak.firsts(ak.all(children_mask,axis=2))),
+            "fj_H_VV_unmatched": to_label(~matched_mask),
         }
         genVars = {**genVars, **genVVars, **genVstarVars, **genLabelVars}
 
@@ -322,7 +328,8 @@ def tagger_gen_QCD_matching(
     partons = genparts[get_pid_mask(genparts, [21, 1, 2, 3, 4, 5], ax=1, byall=False)]
     matched_mask = ak.any(fatjets.delta_r(partons) < match_dR, axis=1)
     btoleptons = genparts[
-        get_pid_mask(genparts, [511, 521, 523], ax=1, byall=False) & get_pid_mask(genparts.children, [11, 13])
+        get_pid_mask(genparts, [511, 521, 523], ax=1, byall=False)
+        & get_pid_mask(genparts.children, [11, 13])
     ]
     matched_b = ak.any(fatjets.delta_r(btoleptons) < 0.5, axis=1)
     genLabelVars = {
