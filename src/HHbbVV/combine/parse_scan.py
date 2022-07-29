bad85d025aef395631ea00cb926891c8b6218746
Lint Action <lint-action@samuelmeuli.com>
2022-07-29 15:58:02 +0000

    Fix code style issues with Black

--- src/HHbbVV/combine/parse_scan.py
+++ src/HHbbVV/combine/parse_scan.py
@@ -8,7 +8,7 @@ from os import scandir
 from os.path import exists
 import pandas as pd
 
-scan_dir = '../cards/04_07_scan/'
+scan_dir = "../cards/04_07_scan/"
 
 cut_dirs = [f.path for f in scandir(scan_dir) if f.is_dir()]
 
@@ -22,20 +22,20 @@ for cut_dir in cut_dirs:
         print(f"{cut_dir} Doesn't exist")
         continue
 
-    split = cut_dir.split('_')
+    split = cut_dir.split("_")
     bdt_cut = split[-3]
     bb_cut = split[-1]
 
-    with open(sig_file, 'r') as f:
+    with open(sig_file, "r") as f:
         try:
-            sig = float(f.readlines()[-2].split(' ')[-1])
+            sig = float(f.readlines()[-2].split(" ")[-1])
         except:
             print(f"{cut_dir} Error parsing significance")
             continue
 
-    with open(lim_file, 'r') as f:
+    with open(lim_file, "r") as f:
         try:
-            lim = float(f.readlines()[-5].split(' ')[-1])
+            lim = float(f.readlines()[-5].split(" ")[-1])
         except:
             print(f"{cut_dir} Error parsing limits")
             continue
@@ -44,6 +44,6 @@ for cut_dir in cut_dirs:
 
 print(len(sigs))
 
-sig_table = pd.DataFrame(sigs, columns=['BDT Cut', 'Txbb Cut', 'Sign.', 'Limit'])
-sig_table.sort_values(['BDT Cut', 'Txbb Cut'], inplace=True)
+sig_table = pd.DataFrame(sigs, columns=["BDT Cut", "Txbb Cut", "Sign.", "Limit"])
+sig_table.sort_values(["BDT Cut", "Txbb Cut"], inplace=True)
 sig_table.to_csv(f"{scan_dir}/signs.csv", index=False)
