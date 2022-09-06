0a9ea61445a3cfde3c6f0a25709fe5c1b9f53c47
Lint Action <lint-action@samuelmeuli.com>
2022-09-06 13:50:10 +0000

    Fix code style issues with Black

--- src/condor/check_jobs.py
+++ src/condor/check_jobs.py
@@ -9,10 +9,13 @@ eosdir = f"/eos/uscms/store/user/rkansal/bbVV/skimmer/{tag}/2017/"
 samples = listdir(eosdir)
 jdls = [jdl for jdl in listdir(f"condor/{tag}/") if jdl.endswith(".jdl")]
 
-jdl_dict = {sample: np.sort([int(jdl[:-4].split('_')[-1]) for jdl in jdls if sample in jdl])[-1] for sample in samples} 
+jdl_dict = {
+    sample: np.sort([int(jdl[:-4].split("_")[-1]) for jdl in jdls if sample in jdl])[-1]
+    for sample in samples
+}
 
 for sample in samples:
-    outs = [int(out.split('.')[0].split('_')[-1]) for out in listdir(f"{eosdir}/{sample}/parquet")]
+    outs = [int(out.split(".")[0].split("_")[-1]) for out in listdir(f"{eosdir}/{sample}/parquet")]
     print(sample)
     print(outs)
 
@@ -20,4 +23,3 @@ for sample in samples:
         if i not in outs:
             print(i)
             os.system(f"condor_submit condor/{tag}/2017_{sample}_{i}.jdl")
-
