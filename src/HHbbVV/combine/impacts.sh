762835cc35e783edf03724ee185dd6b74242d3a1
rkansal47 <raghavk1997@yahoo.co.in>
2023-03-07 20:39:19 +0530

    add plots

--- src/HHbbVV/combine/impacts.sh
+++ src/HHbbVV/combine/impacts.sh
@@ -24,3 +24,12 @@ combineTool.py -M Impacts -t -1 --snapshotName MultiDimFit --bypassFrequentistFi
 --setParameters mask_pass=1,mask_fail=1,mask_passBlinded=0,mask_failBlinded=0 \
 --freezeParameters CMS_bbWW_boosted_ggf_qcdparam_msdbin5,CMS_bbWW_boosted_ggf_qcdparam_msdbin6,CMS_bbWW_boosted_ggf_qcdparam_msdbin7,CMS_bbWW_boosted_ggf_qcdparam_msdbin8,CMS_bbWW_boosted_ggf_qcdparam_msdbin9 \
 --setParameterRanges r=-15,15 -v 9 2>&1 | tee $outsdir/Impacts.txt
+
+echo "Collect impacts"
+combineTool.py -M Impacts -d ${wsm_snapshot}.root -m 125 -n ".impacts" \
+--setParameters mask_pass=1,mask_fail=1,mask_passBlinded=0,mask_failBlinded=0 \
+--freezeParameters CMS_bbWW_boosted_ggf_qcdparam_msdbin5,CMS_bbWW_boosted_ggf_qcdparam_msdbin6,CMS_bbWW_boosted_ggf_qcdparam_msdbin7,CMS_bbWW_boosted_ggf_qcdparam_msdbin8,CMS_bbWW_boosted_ggf_qcdparam_msdbin9 \
+--setParameterRanges r=-20,20 -o impacts.json
+
+echo "Plot impacts"
+plotImpacts.py -i impacts.json -o impacts
