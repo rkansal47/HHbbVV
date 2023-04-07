78a7203e53eddde93d177f0e033eded0e9fb2eb7
rkansal47 <raghavk1997@yahoo.co.in>
2023-04-07 15:57:08 +0200

    gen selection

--- paper/triggers.py
+++ paper/triggers.py
@@ -3,3 +3,59 @@ muon_HLTs = {
     "2017": ["IsoMu27", "Mu50"],
     "2018": ["IsoMu24", "Mu50"],
 }
+
+HLTs = {
+    "2016": [
+        "AK8DiPFJet250_200_TrimMass30_BTagCSV_p20",
+        "AK8DiPFJet280_200_TrimMass30_BTagCSV_p20",
+        #
+        "AK8PFHT600_TrimR0p1PT0p03Mass50_BTagCSV_p20",
+        "AK8PFHT700_TrimR0p1PT0p03Mass50",
+        #
+        "AK8PFJet360_TrimMass30",
+        "AK8PFJet450",
+        "PFJet450",
+        #
+        "PFHT800",
+        "PFHT900",
+        "PFHT1050",
+        #
+        "PFHT750_4JetPt50",
+        "PFHT750_4JetPt70",
+        "PFHT800_4JetPt50",
+    ],
+    "2017": [
+        "PFJet450",
+        "PFJet500",
+        #
+        "AK8PFJet400",
+        "AK8PFJet450",
+        "AK8PFJet500",
+        #
+        "AK8PFJet360_TrimMass30",
+        "AK8PFJet380_TrimMass30",
+        "AK8PFJet400_TrimMass30",
+        #
+        "AK8PFHT750_TrimMass50",
+        "AK8PFHT800_TrimMass50",
+        #
+        "PFHT1050",
+        #
+        "AK8PFJet330_PFAK8BTagCSV_p17",
+    ],
+    "2018": [
+        "PFJet500",
+        #
+        "AK8PFJet500",
+        #
+        "AK8PFJet360_TrimMass30",
+        "AK8PFJet380_TrimMass30",
+        "AK8PFJet400_TrimMass30",
+        "AK8PFHT750_TrimMass50",
+        "AK8PFHT800_TrimMass50",
+        #
+        "PFHT1050",
+        #
+        "HLT_AK8PFJet330_TrimMass30_PFAK8BTagCSV_p17_v",
+    ],
+}
