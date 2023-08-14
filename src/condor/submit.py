1e8028de91dceeb5bf37e9e9fd13d2c15e45daa3
Andres Nava <andresnava1000@gmail.com>
2023-08-14 14:09:28 -0700

    fixing earlier issues on with push

--- src/condor/submit.py
+++ src/condor/submit.py
@@ -43,8 +43,9 @@ def main(args):
     elif args.site == "ucsd":
         t2_local_prefix = "/ceph/cms/"
         t2_prefix = "root://redirector.t2.ucsd.edu:1095"
-        proxy = "/home/users/rkansal/x509up_u31735"
-
+        proxy = "/home/users/annava/projects/HHbbVV/test" # edited this to be this for Andres: grid-proxy-init -debug -verify 
+        print(f'given proxy?',proxy) # debug
+        
     username = os.environ["USER"]
     local_dir = f"condor/{args.processor}/{args.tag}"
     homedir = f"/store/user/{username}/bbVV/{args.processor}/"
