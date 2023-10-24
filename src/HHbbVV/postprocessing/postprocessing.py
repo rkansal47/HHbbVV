de9f3a3ddf4295a728f32440b05cb925f50ec009
Andres Nava <andresnava1000@gmail.com>
2023-10-23 17:51:12 -0700

    new branch where the skimmer is modified to include weight information to study strange TT background in vbf search

--- src/HHbbVV/postprocessing/postprocessing.py
+++ src/HHbbVV/postprocessing/postprocessing.py
@@ -429,9 +429,9 @@ def main(args):
                     )
             
             _add_vbf_columns(df,bb_mask,ptlabel="",mlabel="")
-            
+
             if key == 'Data':
-                break
+                continue
 
             for var in pt_labels:
                 for direction in ["down", "up"]:
