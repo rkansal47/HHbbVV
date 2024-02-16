14098a5d60f822a03aa06eeaa819d306b2e59bbb
rkansal47 <raghavk1997@yahoo.co.in>
2024-02-15 19:15:35 -0600

    minor

--- src/HHbbVV/processors/bbVVSkimmer.py
+++ src/HHbbVV/processors/bbVVSkimmer.py
@@ -634,7 +634,6 @@ class bbVVSkimmer(processor.ProcessorABC):
             if dataset in self.XSECS or "XToYHTo2W2BTo4Q2B" in dataset:
                 # 1 fb xsec for now for resonant signal
                 xsec = self.XSECS[dataset] if dataset in self.XSECS else 1e-3  # in pb
-
                 weight_norm = xsec * LUMI[year]
             else:
                 logger.warning("Weight not normalized to cross section")
