bug fixes and postprocessing normalization

src/HHbbVV/postprocessing/postprocessing.py
@@ -46,6 +46,7 @@ from hh_vars import (
     samples,
     nonres_samples,
     res_samples,
+    norm_preserving_weights,
     txbb_wps,
     jec_shifts,
     jmsr_shifts,
@@ -350,6 +351,7 @@ nonres_sig_keys_ggf = [
 # TODO: check which of these applies to resonant as well
 weight_shifts = {
     "pileup": Syst(samples=nonres_sig_keys + res_sig_keys + bg_keys, label="Pileup"),
+    "pileupID": Syst(samples=nonres_sig_keys + res_sig_keys + bg_keys, label="Pileup ID"),
     # "PDFalphaS": Syst(samples=nonres_sig_keys, label="PDF"),
     # "QCDscale": Syst(samples=nonres_sig_keys, label="QCDscale"),
     "ISRPartonShower": Syst(samples=nonres_sig_keys_ggf + ["V+Jets"], label="ISR Parton Shower"),
@@ -810,6 +812,120 @@ def _make_dirs(args, scan, scan_cuts, scan_wps):
             )
 
 
+def normalize_weights(events: pd.DataFrame, totals: Dict, sample: str, isData: bool):
+    """Normalize weights and all the variations"""
+
+    # don't need any reweighting for data
+    if isData:
+        events["finalWeight"] = events["weight"]
+        return
+
+    # check weights are scaled
+    if "weight_noxsec" in events:
+        if np.all(events["weight"] == events["weight_noxsec"]):
+            warnings.warn(f"{sample} has not been scaled by its xsec and lumi!")
+
+    # checking that trigger efficiencies have been applied
+    if "weight_noTrigEffs" in events and not np.all(
+        np.isclose(events["weight"], events["weight_noTrigEffs"], rtol=1e-5)
+    ):
+        # normalize weights with and without trigger efficiencies
+        events["finalWeight"] = events["weight"] / totals["np_nominal"]
+        events["weight_noTrigEffs"] /= totals["np_nominal"]
+    else:
+        events["weight"] /= totals["np_nominal"]
+
+    # normalize all the variations
+    for wvar in weight_shifts:
+        if f"weight_{wvar}Up" not in events:
+            continue
+
+        for shift in ["Up", "Down"]:
+            wlabel = wvar + shift
+            if wvar in norm_preserving_weights:
+                # normalize by their totals
+                events[f"weight_{wlabel}"] /= totals[f"np_{wlabel}"]
+            else:
+                # normalize by the nominal
+                events[f"weight_{wlabel}"] /= totals[f"np_nominal"]
+
+    # normalize scale and PDF weights
+    for wkey in ["scale_weights", "pdf_weights"]:
+        if wkey in events:
+            events[wkey] /= totals[f"np_{wkey}"]
+
+
+def load_samples(
+    data_dir: str,
+    samples: Dict[str, str],
+    year: str,
+    filters: List = None,
+    columns: List = None,
+    hem_cleaning: bool = True,
+) -> Dict[str, pd.DataFrame]:
+    """
+    Loads events with an optional filter.
+    Divides MC samples by the total pre-skimming, to take the acceptance into account.
+
+    Args:
+        data_dir (str): path to data directory.
+        samples (Dict[str, str]): dictionary of samples and selectors to load.
+        year (str): year.
+        filters (List): Optional filters when loading data.
+        columns (List): Optional columns to load.
+        hem_cleaning (bool): Whether to apply HEM cleaning to 2018 data.
+
+    Returns:
+        Dict[str, pd.DataFrame]: ``events_dict`` dictionary of events dataframe for each sample.
+
+    """
+    data_dir = Path(data_dir) / year
+    full_samples_list = os.listdir(data_dir)  # get all directories in data_dir
+    events_dict = {}
+
+    # label - key of sample in events_dict
+    # selector - string used to select directories to load in for this sample
+    for label, selector in samples.items():
+        events_dict[label] = []  # list of directories we load in for this sample
+        for sample in full_samples_list:
+            # check if this directory passes our selector string
+            if not utils.check_selector(sample, selector):
+                continue
+
+            sample_path = data_dir / sample
+            parquet_path, pickles_path = sample_path / "parquet", sample_path / "pickles"
+
+            # no parquet directory?
+            if not parquet_path.exists():
+                warnings.warn(f"No parquet directory for {sample}!")
+                continue
+
+            # print(f"Loading {sample}")
+            events = pd.read_parquet(parquet_path, filters=filters, columns=columns)
+
+            # no events?
+            if not len(events):
+                warnings.warn(f"No events for {sample}!")
+                continue
+
+            # normalize by total events
+            totals = utils.get_pickles(pickles_path, year, sample)["totals"]
+            normalize_weights(events, totals, sample, isData=label == data_key)
+
+            if year == "2018" and hem_cleaning:
+                events = utils._hem_cleaning(sample, events)
+
+            events_dict[label].append(events)
+            print(f"Loaded {sample: <50}: {len(events)} entries")
+
+        if len(events_dict[label]):
+            events_dict[label] = pd.concat(events_dict[label])
+        else:
+            del events_dict[label]
+
+    return events_dict
+
+
 def _check_load_systematics(systs_file: str, year: str):
     if os.path.exists(systs_file):
         print("Loading systematics")
@@ -832,15 +948,13 @@ def _load_samples(args, samples, sig_samples, cutflow, filters=None):
     for d in args.signal_data_dirs:
         events_dict = {
             **events_dict,
-            **utils.load_samples(
-                d, sig_samples, args.year, filters, hem_cleaning=args.hem_cleaning
-            ),
+            **load_samples(d, sig_samples, args.year, filters, hem_cleaning=args.hem_cleaning),
         }
 
     if args.data_dir:
         events_dict = {
             **events_dict,
-            **utils.load_samples(
+            **load_samples(
                 args.data_dir, samples, args.year, filters, hem_cleaning=args.hem_cleaning
             ),
         }

src/HHbbVV/processors/bbVVSkimmer.py
@@ -16,7 +16,7 @@ import pathlib
 import pickle, json, gzip
 import os
 
-from typing import Dict
+from typing import Dict, Tuple
 from collections import OrderedDict
 
 from .GenSelection import gen_selection_HHbbVV, gen_selection_HH4V, gen_selection_HYbbVV, G_PDGID
@@ -56,8 +56,9 @@ gen_selection_dict = {
 
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger("bbVVSkimmer")
+# logger.setLevel(logging.INFO)
 
 
 class bbVVSkimmer(processor.ProcessorABC):
@@ -155,9 +156,9 @@ class bbVVSkimmer(processor.ProcessorABC):
         xsecs={},
         save_ak15=False,
         save_systematics=True,
+        lp_sfs=True,
         inference=True,
         save_all=False,
-        vbf_search=True,
     ):
         super(bbVVSkimmer, self).__init__()
 
@@ -167,14 +168,14 @@ class bbVVSkimmer(processor.ProcessorABC):
         # save systematic variations
         self._systematics = save_systematics
 
+        # save LP SFs
+        self._lp_sfs = lp_sfs
+
         # run inference
         self._inference = inference
 
         # save all branches or only necessary ones
         self._save_all = save_all
-
-        # search for VBF production events
-        self._vbf_search = vbf_search
 
         # for tagger model and preprocessing dict
         self.tagger_resources_path = (
@@ -189,8 +190,8 @@ class bbVVSkimmer(processor.ProcessorABC):
 
         self._accumulator = processor.dict_accumulator({})
 
-        logger.info(
-            f"Running skimmer with inference {self._inference} and systematics {self._systematics} and save all {self._save_all} and VBF search {self._vbf_search}"
+        logging.info(
+            f"Running skimmer with inference {self._inference} and systematics {self._systematics} and save all {self._save_all}."
         )
 
     def to_pandas(self, events: Dict[str, np.array]):
@@ -286,9 +287,12 @@ class bbVVSkimmer(processor.ProcessorABC):
                 skimmed_events = {**skimmed_events, **vars_dict}
 
         # used for normalization to cross section below
-        gen_selected = selection.all(*selection.names)
-        print("gen selected", gen_selected)
-        print(f"num gen selected {np.sum(gen_selected)} / {len(events)}")
+        gen_selected = (
+            selection.all(*selection.names)
+            if len(selection.names)
+            else np.ones(len(events)).astype(bool)
+        )
+        logging.info(f"Passing gen selection: {np.sum(gen_selected)} / {len(events)}")
 
         # FatJet vars
 
@@ -418,7 +422,7 @@ class bbVVSkimmer(processor.ProcessorABC):
         if isData:
             for trigger in HLTs[year_nosuffix]:
                 if trigger not in events.HLT.fields:
-                    logger.warning(f"Missing HLT {trigger}!")
+                    logging.warning(f"Missing HLT {trigger}!")
 
             HLT_triggered = np.any(
                 np.array(
@@ -634,25 +638,27 @@ class bbVVSkimmer(processor.ProcessorABC):
         # Weights
         ######################
 
+        totals_dict = {"nevents": n_events}
+
         if isData:
             skimmed_events["weight"] = np.ones(n_events)
         else:
-            skimmed_events = {
-                **skimmed_events,
-                **self.add_weights(
-                    events,
-                    year,
-                    dataset,
-                    gen_weights,
-                    fatjets,
-                    num_jets,
-                    vbf_jets,
-                    goodelectronsHbb,
-                    goodelectronsHH,
-                    goodmuonsHbb,
-                    goodmuonsHH,
-                ),
-            }
+            weights_dict, totals_temp = self.add_weights(
+                events,
+                year,
+                dataset,
+                gen_weights,
+                fatjets,
+                num_jets,
+                vbf_jets,
+                gen_selected,
+                goodelectronsHbb,
+                goodelectronsHH,
+                goodmuonsHbb,
+                goodmuonsHH,
+            )
+            skimmed_events = {**skimmed_events, **weights_dict}
+            totals_dict = {**totals_dict, **totals_temp}
 
         ##############################
         # Reshape and apply selections
@@ -671,7 +677,7 @@ class bbVVSkimmer(processor.ProcessorABC):
         # Lund plane SFs
         ################
 
-        if isSignal and self._systematics:
+        if isSignal and self._systematics and self._lp_sfs:
             # TODO: remember to add new LP variables
             items = [
                 ("lp_sf_lnN", 101),
@@ -749,7 +755,7 @@ class bbVVSkimmer(processor.ProcessorABC):
                 sf_dicts = concatenate_dicts(sf_dicts)
 
             else:
-                print("No signal events selected")
+                logging.info("No signal events selected")
                 sf_dicts = {}
                 for key, shape in items:
                     arr = np.ones((np.sum(sel_all), shape))
@@ -794,7 +800,7 @@ class bbVVSkimmer(processor.ProcessorABC):
         fname = events.behavior["__events_factory__"]._partition_key.replace("/", "_") + ".parquet"
         self.dump_table(df, fname)
 
-        return {year: {dataset: {"nevents": n_events, "cutflow": cutflow}}}
+        return {year: {dataset: {"totals": totals_dict, "cutflow": cutflow}}}
 
     def postprocess(self, accumulator):
         return accumulator
@@ -821,7 +827,7 @@ class bbVVSkimmer(processor.ProcessorABC):
             xsec = self.XSECS[dataset] if dataset in self.XSECS else 1e-3  # in pb
             weight_norm = xsec * LUMI[year]
         else:
-            logger.warning("Weight not normalized to cross section")
+            logging.warning("Weight not normalized to cross section")
             weight_norm = 1
 
         return weight_norm
@@ -840,7 +846,8 @@ class bbVVSkimmer(processor.ProcessorABC):
         goodelectronsHH,
         goodmuonsHbb,
         goodmuonsHH,
-    ):
+    ) -> Tuple[Dict, Dict]:
+        """Adds weights and variations, saves totals for all norm preserving weights and variations"""
         weights = Weights(len(events), storeIndividual=True)
         weights.add("genweight", gen_weights)
 
@@ -858,7 +865,7 @@ class bbVVSkimmer(processor.ProcessorABC):
                 events.L1PreFiringWeight.Dn,
             )
 
-        logger.debug("weights ", weights._weights.keys())
+        logging.debug("weights ", weights._weights.keys())
 
         ###################### Save all the weights and variations ######################
 
@@ -877,8 +884,8 @@ class bbVVSkimmer(processor.ProcessorABC):
         weights_dict["weight_noTrigEffs"] = weights.partial_weight(exclude=["trig_effs"])
 
         # norm preserving weights, used to do normalization in post-processing
-        weights_dict["weight_np"] = weights.weight(include=norm_preserving_weights)
-        totals_dict["np_nominal"] = np.sum(weights_dict["weight_np"][gen_selected])
+        weight_np = weights.partial_weight(include=norm_preserving_weights)
+        totals_dict["np_nominal"] = np.sum(weight_np[gen_selected])
 
         # variations
         if self._systematics:
@@ -886,30 +893,28 @@ class bbVVSkimmer(processor.ProcessorABC):
                 weights_dict[f"weight_{systematic}"] = weights.weight(modifier=systematic)
 
                 if utils.remove_variation_suffix(systematic) in norm_preserving_weights:
-                    weights_dict[f"weight_np_{systematic}"] = weights.partial_weight(
+                    var_weight = weights.partial_weight(
                         include=norm_preserving_weights, modifier=systematic
                     )
 
                     # need to save total # events for each variation for normalization in post-processing
-                    totals_dict[f"np_{systematic}"] = weights_dict[f"weight_np_{systematic}"][
-                        gen_selected
-                    ]
+                    totals_dict[f"np_{systematic}"] = np.sum(var_weight[gen_selected])
 
         ###################### alpha_S and PDF variations ######################
 
         # alpha_s variations, only for HH and ttbar
         if "GluGluToHHTobbVV" in dataset or "VBF_HHTobbVV" in dataset or dataset.startswith("TTTo"):
             scale_weights = get_scale_weights(events)
-            weights_dict["scale_weights"] = scale_weights * weights_dict["weight"]
+            weights_dict["scale_weights"] = scale_weights * weights_dict["weight"][:, np.newaxis]
             totals_dict["np_scale_weights"] = np.sum(
-                (scale_weights * weights_dict["weight_np"])[gen_selected]
+                (scale_weights * weight_np[:, np.newaxis])[gen_selected], axis=0
             )
 
         if "GluGluToHHTobbVV" in dataset or "VBF_HHTobbVV" in dataset:
             pdf_weights = get_pdf_weights(events)
-            weights_dict["pdf_weights"] = pdf_weights * weights_dict["weight"]
+            weights_dict["pdf_weights"] = pdf_weights * weights_dict["weight"][:, np.newaxis]
             totals_dict["np_pdf_weights"] = np.sum(
-                (pdf_weights * weights_dict["weight_np"])[gen_selected]
+                (pdf_weights * weight_np[:, np.newaxis])[gen_selected], axis=0
             )
 
         ###################### Normalization (Step 1) ######################
@@ -942,10 +947,9 @@ class bbVVSkimmer(processor.ProcessorABC):
             + list(lepton_weights._modifiers.items())
         }
 
-        print("lepton weights", lepton_weights_dict)
         weights_dict = {**weights_dict, **lepton_weights_dict}
 
-        return weights_dict
+        return weights_dict, totals_dict
 
     def getDijetVars(
         self, ak8FatJetVars: Dict, bb_mask: np.ndarray, pt_shift: str = None, mass_shift: str = None

src/HHbbVV/processors/corrections.py
@@ -251,7 +251,7 @@ def get_scale_weights(events):
     [1] is renscfact=0.5d0 facscfact=1d0 ; <=
     [2] is renscfact=0.5d0 facscfact=2d0 ;
     [3] is renscfact=1d0 facscfact=0.5d0 ; <=
-    [4] is renscfact=1d0 facscfact=1d0 ;
+    [4] is renscfact=1d0 facscfact=1d0 ; <= saving this one as a formality, empirically they seem to be all 1s as expected
     [5] is renscfact=1d0 facscfact=2d0 ; <=
     [6] is renscfact=2d0 facscfact=0.5d0 ;
     [7] is renscfact=2d0 facscfact=1d0 ; <=
@@ -259,11 +259,7 @@ def get_scale_weights(events):
 
     See also https://git.rwth-aachen.de/3pia/cms_analyses/common/-/blob/11e0c5225416a580d27718997a11dc3f1ec1e8d1/processor/generator.py#L93 for an example.
     """
-    print("Scale weights: ")
-    print(events.LHEScaleWeight[:, [0, 1, 3, 5, 7, 8, 4]])
-    print("TODO: double check, last column should be 1s!!")
-
-    return events.LHEScaleWeight[:, [0, 1, 3, 5, 7, 8]].to_numpy()
+    return events.LHEScaleWeight[:, [0, 1, 3, 5, 7, 8, 4]].to_numpy()
 
 
 def _btagSF(cset, jets, flavour, wp="M", algo="deepJet", syst="central"):
@@ -339,7 +335,7 @@ def add_pileupid_weights(weights: Weights, year: str, jets: JetArray, genjets, w
         # product of SFs across arrays, automatically defaults empty lists to 1
         sfs_var.append(ak.prod(sfs, axis=1))
 
-    weights.add("pileupIDSF", *sfs_var)
+    weights.add("pileupID", *sfs_var)
 
 
 # for scale factor validation region selection
@@ -450,9 +446,6 @@ def add_lepton_id_weights(
         val = 1 - np.prod(1 - value, axis=1)
         val[~lep_exists] = 1  # if no leps in event, SF = 1
         values[key] = np.nan_to_num(val, nan=1)
-
-    print(lepton_type, label)
-    print(values)
 
     # add weights (for now only the nominal weight)
     weights.add(f"{lepton_type}{label}_id_{wp}", values["nominal"], values["up"], values["down"])

src/HHbbVV/processors/utils.py
@@ -204,7 +204,7 @@ class Weights:
             return self._weight / self._modifiers[modifier.replace("Down", "Up")]
         return self._weight * self._modifiers[modifier]
 
-    def partial_weight(self, include=[], exclude=[]):
+    def partial_weight(self, include=[], exclude=[], modifier=None):
         """Partial event weight vector
 
         Return a partial weight by multiplying a subset of all weights.
@@ -219,9 +219,12 @@ class Weights:
                 Weight names to include, defaults to []
             exclude : list
                 Weight names to exclude, defaults to []
+            modifier : str, optional
+                if supplied, provide event weight corresponding to a particular
+                systematic uncertainty shift, of form ``str(name + 'Up')`` or (Down)
         Returns
         -------
-            weight : np.ndarray
+            weight : numpy.ndarray
                 The weight vector, corresponding to only the effect of the
                 corrections specified.
         """
@@ -231,10 +234,6 @@ class Weights:
             )
         if (include and exclude) or not (include or exclude):
             raise ValueError("Need to specify exactly one of the 'exclude' or 'include' arguments.")
-        if include and not isinstance(include, list):
-            raise ValueError("'include' should be a list of weight names")
-        if exclude and not isinstance(exclude, list):
-            raise ValueError("'exclude' should be a list of weight names")
 
         names = set(self._weights.keys())
         if include:
@@ -246,7 +245,13 @@ class Weights:
         for name in names:
             w = w * self._weights[name]
 
-        return w
+        if modifier is None:
+            return w
+        elif modifier.replace("Down", "").replace("Up", "") not in names:
+            raise ValueError(f"Modifier {modifier} is not in the list of included weights")
+        elif "Down" in modifier and modifier not in self._modifiers:
+            return w / self._modifiers[modifier.replace("Down", "Up")]
+        return w * self._modifiers[modifier]
 
     @property
     def variations(self):