7ce27f0cfc82b4722cc9f11d538c9dfdeaa5fddb
rkansal47 <raghavk1997@yahoo.co.in>
2024-04-28 11:33:13 -0500

    scan script update

--- src/HHbbVV/combine/nonres_scan.sh
+++ src/HHbbVV/combine/nonres_scan.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 # shellcheck disable=SC2043
 
+templates_tag="24Apr9ggFScan"
+sigsample="qqHH_CV_1_C2V_0_kl_1_HHbbVV"
+# sigsample="HHbbVV"
+
+if [[ $sigsample == "qqHH_CV_1_C2V_0_kl_1_HHbbVV" ]]
+then
+  sig_tag="vbf-sig-only"
+else
+  sig_tag="ggf-sig-only"
+fi
+
+for bdt_cut in 0.996 # 0.995 0.9965 # 0.9997 0.9999
+do
+  for txbb_cut in "MP" "HP"
+  do
+    if [[ $sigsample == "qqHH_CV_1_C2V_0_kl_1_HHbbVV" ]]
+    then
+      cutstr="ggf_txbb_MP_ggf_bdt_0.9965_vbf_txbb_${txbb_cut}_vbf_bdt_${bdt_cut}_lepton_veto_Hbb"
+    else
+      cutstr="ggf_txbb_${txbb_cut}_ggf_bdt_${bdt_cut}_vbf_txbb_HP_vbf_bdt_0.999_lepton_veto_Hbb"
+    fi
+    echo $cutstr
+
+    python3 -u postprocessing/CreateDatacard.py --templates-dir templates/$templates_tag/$cutstr \
+    --model-name $templates_tag/$sig_tag/$cutstr --no-do-jshifts --sig-sample $sigsample
+
+    (
+      cd cards/$templates_tag/$sig_tag/$cutstr || exit
+      /uscms/home/rkansal/nobackup/HHbbVV/src/HHbbVV/combine/run_blinded.sh -wbl
+    )
+  done
+done
+
+
 # for lepton_veto in "None" "Hbb" "HH"
 # do
 #   cutstr="txbb_MP_bdt_0.998_lepton_veto_${lepton_veto}"
@@ -14,34 +48,3 @@
 #     /uscms/home/rkansal/nobackup/HHbbVV/src/HHbbVV/combine/run_blinded.sh -wbl
 #   )
 # done
-
-templates_tag="24Apr9ggFScan"
-nTF=0
-# sigsample="qqHH_CV_1_C2V_0_kl_1_HHbbVV"
-sigsample="HHbbVV"
-
-for bdt_cut in 0.996 #m0.995 0.9965 # 0.9997 0.9999
-do
-  for txbb_cut in "MP" "HP"
-  do
-    cutstr="ggf_txbb_${txbb_cut}_ggf_bdt_${bdt_cut}_vbf_txbb_HP_vbf_bdt_0.999_lepton_veto_Hbb"
-    echo $cutstr
-
-    # python3 -u postprocessing/CreateDatacard.py --templates-dir templates/$templates_tag/$cutstr \
-    # --model-name $templates_tag/SM/$cutstr --no-do-jshifts --nTF 0 --only-sm
-
-    # (
-    #   cd cards/$templates_tag/SM/$cutstr || exit
-    #   /uscms/home/rkansal/nobackup/HHbbVV/src/HHbbVV/combine/run_blinded.sh -wbl
-    # )
-
-    model_name=${templates_tag}_nTF$nTF/$cutstr
-    python3 -u postprocessing/CreateDatacard.py --templates-dir templates/$templates_tag/$cutstr \
-    --model-name $model_name --no-do-jshifts --nTF $nTF --sig-sample $sigsample
-
-    (
-      cd cards/$model_name || exit
-      /uscms/home/rkansal/nobackup/HHbbVV/src/HHbbVV/combine/run_blinded.sh -wbl
-    )
-  done
-done
